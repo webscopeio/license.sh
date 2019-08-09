7c09f1145b278394a9eeb40338b35b623007f061
Jan Vorcak <vorcak@webscope.io>
2019-08-09 17:43:17 +0200

    Changed the way how we execute the script

--- license-sh.py
+++ license-sh.py
@@ -3,52 +3,42 @@
 
 Usage:
   license-sh.py
-  license-sh.py init
-  license-sh.py ship <name> move <x> <y> [--speed=<kn>]
-  license-sh.py ship shoot <x> <y>
-  license-sh.py mine (set|remove) <x> <y> [--moored | --drifting]
-  license-sh.py (-t | --tree)
+  license-sh.py <path>
   license-sh.py (-h | --help)
   license-sh.py --version
 
 Options:
   -h --help     Show this screen.
   --version     Show version.
-  --speed=<kn>  Speed in knots [default: 10].
-  --moored      Moored (anchored) mine.
-  --drifting    Drifting mine.
-
 """
+
+VERSION = "1.0.0"
+
 import json
 
 from docopt import docopt
+
+from license_sh.project_identifier import get_project_types, ProjectType
 from license_sh.runners.npm import NpmRunner
 from license_sh.runners.python import PythonRunner
 
 if __name__ == '__main__':
-  arguments = docopt(__doc__, version='License.sh')
-  if arguments['init']:
-    print('Init')
-    exit(0)
+  arguments = docopt(__doc__, version=VERSION)
 
-  # print(arguments)
+  path = arguments['<path>'] or '.'
 
   try:
     with open('./.license-sh.json') as license_file:
       config = json.load(license_file)
-      for project in config['projects']:
-        directory = project['directory']
-        project_type = project['type']
 
-        # skip if requested
-        if project.get('skip', False):
-          print(f"⚠️  Skipping {directory}")
-          continue
+      project_types = get_project_types(path)
 
-        if project_type == 'npm':
-          runner = NpmRunner(directory, config)
-        if project_type == 'python':
-          runner = PythonRunner(directory, config)
+      if ProjectType.PYTHON_PIPENV in project_types:
+        runner = PythonRunner(path, config)
+        runner.check()
+
+      if ProjectType.NPM in project_types:
+        runner = NpmRunner(path, config)
         runner.check()
 
   except FileNotFoundError:
@@ -59,4 +49,3 @@ if __name__ == '__main__':
   except PermissionError:
     print('No permission to read the file .license-sh.json')
     exit(2)
-
--- license_sh/project_identifier/__init__.py
+++ license_sh/project_identifier/__init__.py
@@ -1,7 +1,32 @@
-class ProjectIdentifier:
+import os
+from os import access, R_OK
+from os.path import isfile
+from enum import Enum
 
-  # Python
-  PYTHON_REQUIREMENTS = 'PYTHON_REQUIREMENTS'
-  PYTHON_PIPENV = 'PYTHON_PIPENV'
+
+class ProjectType(Enum):
+  PYTHON_PIPENV = 'python_pipenv'
+  NPM = 'npm'
 
 
+__file_exists = lambda path: isfile(path) and access(path, R_OK)
+
+
+def get_project_types(dir_path: str):
+  types = []
+
+  # check for python's pipenv
+  pipfile_path: str = os.path.join(dir_path, 'Pipfile')
+  pipfile_lock_path: str = os.path.join(dir_path, 'Pipfile.lock')
+
+  if __file_exists(pipfile_path) and __file_exists(pipfile_lock_path):
+    types += [ProjectType.PYTHON_PIPENV]
+
+  # check for npm
+  pipfile_path: str = os.path.join(dir_path, 'package.json')
+  pipfile_lock_path: str = os.path.join(dir_path, 'package-lock.json')
+
+  if __file_exists(pipfile_path) and __file_exists(pipfile_lock_path):
+    types += [ProjectType.NPM]
+
+  return types
