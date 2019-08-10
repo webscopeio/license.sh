0b15f829017c126287d4ac488e6ffa3aa12ac56f
Jan Vorcak <vorcak@webscope.io>
2019-08-10 13:14:59 +0200

    License-sh now runs in it's CWD only

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='License.sh',
+  version='1.0.0',
+  description='Verify software licenses of your open source software',
+  author='Jan Vorcak',
+  author_email='vorcak@webscope.io',
+  url='https://license.sh',
+  scripts=['license-sh'],
+  packages=[
+    'license_sh',
+    'license_sh.project_identifier',
+    'license_sh.runners',
+    'license_sh.runners.npm',
+    'license_sh.runners.python',
+    'license_sh.reporters',
+  ],
+  install_requires=[
+    "anytree",
+    "yaspin",
+    "aiohttp",
+    "PyInquirer",
+    "urllib3",
+    "docopt",
+  ]
+)
