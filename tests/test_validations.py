270dfafaf4e7b7f096775239ec796882615d7112
Jan Vorcak <vorcak@webscope.io>
2019-10-08 15:44:14 +0200

    Support for SPDX license expressions (without WITH) operator

--- tests/test_validations.py
+++ tests/test_validations.py
@@ -1,17 +1,42 @@
 import unittest
 
-from license_sh.helpers import is_license_problem
+from license_sh.helpers import is_license_ok
 
 
 class LicenseValidationsTestCase(unittest.TestCase):
 
   def test_simple_validation_test(self):
     whitelist = ['MIT', 'Apache-2.0']
-    self.assertFalse(is_license_problem('MIT', whitelist), "MIT should not be not problematic, it's in a whitelist")
+    self.assertTrue(is_license_ok('MIT', whitelist), "MIT should not be not problematic, it's in a whitelist")
 
   def test_license_in_comma_separated_expression(self):
     whitelist = ['MIT', 'Apache-2.0']
-    self.assertFalse(is_license_problem('MIT, Apache 2.0', whitelist), "MIT should not be not problematic, it's in a whitelist")
+    # license.sh doesn't support commas, we support SPDX syntax
+    self.assertEqual(is_license_ok('MIT, Apache 2.0', whitelist), None)
+
+  def test_and_license_expression(self):
+    whitelist = ['MIT', 'Apache-2.0']
+    self.assertTrue(is_license_ok('(MIT AND Apache-2.0)', whitelist))
+    self.assertFalse(is_license_ok('(MIT AND Apache-3.0)', whitelist))
+
+  def test_or_license_expression(self):
+    whitelist = ['MIT', 'Apache-3.0']
+    self.assertTrue(is_license_ok('(MIT OR Apache-2.0)', whitelist))
+
+  def test_complex_or_license_expression(self):
+    whitelist = ['MIT', 'Apache-3.0']
+    self.assertTrue(is_license_ok('((MIT and Apache-3.0) OR Apache-2.0)', whitelist))
+    self.assertTrue(is_license_ok('((License-2 or Apache-3.0) OR Apache-2.0)', whitelist))
+    self.assertFalse(is_license_ok('((License-2 and Apache-3.0) OR Apache-2.0)', whitelist))
+
+  def test_nested_invalid_expression_inside_complex_or_license_expression(self):
+    whitelist = ['MIT', 'Apache-3.0']
+    self.assertFalse(is_license_ok('((License-2, Apache-3.0) OR Apache-2.0)', whitelist))
+
+  def test_multi_operator_expression(self):
+    whitelist = ['MIT']
+    self.assertEqual(is_license_ok('(BSD-2-Clause OR MIT OR Apache-2.0)', whitelist), True)
+
 
 if __name__ == '__main__':
   unittest.main()
