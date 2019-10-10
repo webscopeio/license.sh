8caf6e917a245c916b1e133d6cf8637ca6a8881c
Bogdan Sikora <sikora.bogdan@protonmail.com>
2019-10-10 13:14:03 +0200

    fix(yarn): dependency tree fixed

--- license_sh/runners/yarn/__init__.py
+++ license_sh/runners/yarn/__init__.py
@@ -1,49 +1,82 @@
 import json
 import subprocess
+import os
 from os import path
-from anytree import AnyNode, PreOrderIter
+from anytree import AnyNode, PreOrderIter, RenderTree
+from typing import Dict, List
 from yaspin import yaspin
+from pathlib import Path
 
-def get_name_and_version(name):
-  split_name = name.split('@')
-  if len(split_name) < 1:
-    return name, None
-  if name[0] is '@':
-    dep_name = '@' + split_name[-2]
-  else:
-    dep_name = split_name[-2]
-  return dep_name, split_name[-1]
+PARSE_YARN_LOCK_PATH = path.join(Path(__file__).parent, '..', '..', '..', 'js')
+PARSE_YARN_LOCK_SCRIPT = path.join(PARSE_YARN_LOCK_PATH, 'parseYarnLock.js')
 
-def get_dependency_licenses(license_data):
-  result = {}
-  lisenses = license_data.get('data', {}).get('body', []) 
-  for license in lisenses:
-    result[f"{license[0]}@{license[1]}"] = license[2]
-  return result
+def parse_yarn_lock(json_element) -> Dict[str, str]:
+  package_map = {}
+  for (key, dependency) in json_element.get('object', {}).items():
+    package_map[key] = dependency.get('version', None)
+  return package_map
 
-def add_nested_dependencies(dependency, yarn_lock_tree, parent):
-  for child in dependency.get('children', []):
-    dep_name, dep_version = get_name_and_version(child.get('name'))
-    node = AnyNode(name=dep_name, version=dep_version, parent=parent, dependencies=child.get('children'))
-    add_nested_dependencies(child, yarn_lock_tree, node)
+def get_name(name: str):
+  parsedName, signName, *rest = name.split('@')
+  if name.startswith('@'):
+    return '@' + signName
+  return parsedName
 
+def get_flat_tree(dependencies: List, package_map: Dict[str, str]) -> Dict:
+  flat_tree = {}
+  for dependency in dependencies:
+    dep_full_name = dependency.get('name')
+    flat_tree[dependency.get('name')] = {
+      'name': get_name(dep_full_name),
+      'version': package_map.get(dep_full_name),
+      'dependencies': get_flat_tree(dependency.get('children', []), package_map)
+    }
+  return flat_tree
 
-def get_dependency_tree(package_json, yarn_lock_tree):
-  root = AnyNode(name=package_json.get('name', 'package.json'), dependencies=yarn_lock_tree,
+def add_nested_dependencies(dependency, parent):
+  for name, dependency in dependency.get('dependencies', {}).items():
+    dep_name = dependency.get('name')
+    dep_version = dependency.get('version')
+    full_dep_name = f'{dep_name}@{dep_version}'
+    dep_dependencies = dependency.get('dependencies', {})
+    if not dep_version:
+      continue
+    node = AnyNode(name=dep_name, version=dependency.get('version'), parent=parent, dependencies=dep_dependencies)
+
+    dep = None
+    p = node
+    names = []
+    while p.parent:
+      if not dep and p.dependencies and p.dependencies.get(full_dep_name) and p.dependencies.get(full_dep_name).get('dependencies'):
+        node.dependencies = p.dependencies.get(full_dep_name).get('dependencies')
+        dep = p.dependencies.get(full_dep_name)
+      p = p.parent
+      names.append(p.name)
+
+    # check the root
+    if not dep and p.dependencies and p.dependencies.get(full_dep_name) and p.dependencies.get(full_dep_name).get('dependencies'):
+      node.dependencies = p.dependencies.get(full_dep_name).get('dependencies')
+      dep = p.dependencies.get(full_dep_name)
+
+    names = names[:-1]  # let's forget about top level
+    # if I'm not already in a tree
+    if dep_name not in names:
+      if dep:
+        add_nested_dependencies(dep, node)
+
+def get_dependency_tree(flat_tree, package_json, package_map):
+  root = AnyNode(name=package_json.get('name', 'package.json'), dependencies=flat_tree,
                  version=package_json.get('version'))
+  # load root dependencies from package.json
+  for dep_name, dep_version in package_json.get('dependencies', {}).items():
+    resolved_version = package_map.get(f'{dep_name}@{dep_version}')
+    full_dep_name = f'{dep_name}@{resolved_version}'
+    dependency = flat_tree[full_dep_name]
+    version = dependency.get('version')
+    parent = AnyNode(name=dep_name, version=version, parent=root, dependencies=dependency.get('dependencies'))
+    add_nested_dependencies(dependency, parent)
 
-  for dependency in yarn_lock_tree.get('data', {}).get('trees', []):
-    dep_name, dep_version = get_name_and_version(dependency.get('name'))
-    parent = AnyNode(name=dep_name, version=dep_version, parent=root, dependencies=dependency.get('children', []))
-    add_nested_dependencies(dependency, yarn_lock_tree, parent=parent)
   return root
-
-def get_dependency_versions(yarn_lock):
-  version_map = {}
-  for name in yarn_lock.keys():
-    version_map[name] = yarn_lock[name].get('version', None)
-  return version_map
-
 
 class YarnRunner:
   """
@@ -60,7 +93,7 @@ class YarnRunner:
   def check(self):
     with open(self.package_json_path) as package_json_file:
       package_json = json.load(package_json_file)
-      project_name = package_json.get('name', 'package.json')    
+      project_name = package_json.get('name', 'project_name')
 
     if self.verbose:
       print("===========")
@@ -69,48 +102,41 @@ class YarnRunner:
 
 
     with yaspin(text="Analysing dependencies ...") as sp:
-      all_dependencies = json.loads((subprocess.run([
+      tree_list_output = json.loads((subprocess.run([
         'yarn',
         'list',
         '--json',
+        '--silent',
         '--no-progress',
-        '--no-interactive',
         '--cwd',
         self.directory],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
       )).stdout)
-      license_result = subprocess.run([
+      # install yarn lock parser
+      subprocess.run([
         'yarn',
-        'licenses',
-        'list',
-        '--json',
-        '--no-progress',
-        '--no-interactive',
+        'install',
         '--cwd',
-        self.directory],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True
-      ).stdout
-      yarn_lock_result = json.loads(subprocess.run([
+        PARSE_YARN_LOCK_PATH]
+      )
+      # run yarn lock parser
+      yarn_lock_output = json.loads(subprocess.run([
         'node',
-        './js/lockToJson.js',
+        PARSE_YARN_LOCK_SCRIPT,
         f'{self.directory}/yarn.lock'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-      ).stdout).get('object', {})
+        stdout=subprocess.PIPE
+      ).stdout)
+      package_map = parse_yarn_lock(yarn_lock_output)
+      flat_tree = get_flat_tree(tree_list_output.get('data', {}).get('trees', []), package_map)
+      dep_tree = get_dependency_tree(flat_tree, package_json, package_map)
+
+      license_map = {} # TODO: Add license map
       
       
-      dep_tree = get_dependency_tree(package_json, all_dependencies)
-      for line in license_result.split('\n'):
-        if '{"type":"table"' in line:
-          license_map = get_dependency_licenses(json.loads(line))
-      version_map = get_dependency_versions(yarn_lock_result)
-      
-      for node in PreOrderIter(dep_tree):
-        delattr(node, 'dependencies')
-        node.version = version_map.get(f'{node.name}@{node.version}', node.version)
-        node.license = license_map.get(f'{node.name}@{node.version}', None)
+    for node in PreOrderIter(dep_tree):
+      delattr(node, 'dependencies')
+      # license_map[f'{node.name}@{node.version}'] = 'MIT' # TODO: Remove
+      node.license = license_map.get(f'{node.name}@{node.version}', None)
 
     return dep_tree, license_map
