427e113727b6fe5f03e806029fcc7e522d1cc33b
John Vandenberg <jayvdb@gmail.com>
2019-12-17 23:08:06 +0700

    setup.py: Exclude tests from installation

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/webscopeio/license.sh",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
