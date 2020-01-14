46bd268e17f564401d1b3e2d0ae1aac4cad1909d
Bogdan Sikora <sikora.bogdan@gmail.com>
2020-01-14 14:09:36 +0100

    feat: get license from specific version in not found

--- license_sh/runners/runners_shared.py
+++ license_sh/runners/runners_shared.py
@@ -3,7 +3,7 @@ import asyncio
 import aiohttp as aiohttp
 
 NPM_HOST = "https://registry.npmjs.org"
-
+UNKNOWN = "Unknown"
 
 def fetch_npm_licenses(all_dependencies):
     license_map = {}
@@ -13,6 +13,13 @@ def fetch_npm_licenses(all_dependencies):
         for dependency, version in all_dependencies
     ]
 
+    def get_license(page, version):
+        # Extract package license from npm json data
+        license_name = page.get("license", UNKNOWN)
+        if license_name == UNKNOWN:
+            license_name = page.get('versions', {}).get(version, {}).get("license", UNKNOWN)
+        return license_name
+    
     async def fetch(session, url, version):
         async with session.get(url) as resp:
             return await resp.text(), version
@@ -24,14 +31,12 @@ def fetch_npm_licenses(all_dependencies):
             tasks = []
             for url, version in urls:
                 tasks.append(loop.create_task(fetch(session, url, version)))
-
             for result in asyncio.as_completed(tasks):
                 try:
                     output, version = await result
                     page = json.loads(output)
-                    license_map[f"{page['name']}@{version}"] = page.get(
-                        "license", "Unknown"
-                    )
+                    license_map[f"{page['name']}@{version}"] = get_license(page, version)
+
                 except json.JSONDecodeError:
                     pass
 
