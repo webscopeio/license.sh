f5de574ab0a793b8aef7840137ea1c04167c84fd
Bogdan Sikora <sikora.bogdan@gmail.com>
2020-02-05 13:06:43 +0100

    feat: config warnings mover to stderr

--- license_sh/config.py
+++ license_sh/config.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from os import path
 from typing import List
 from .project_identifier import ProjectType
@@ -21,12 +22,14 @@ def get_ignored_packages(ignored_packages: dict):
     for project_type, ignored_packages in ignored_packages.items():
         if not project_type in project_list:
             print(
-                f"Ignored packages for project '{project_type}' is not supported. Ignoring..."
+                f"Ignored packages for project '{project_type}' is not supported. Ignoring...",
+                file=sys.stderr,
             )
             continue
         if not isinstance(ignored_packages, list):
             print(
-                f"Ignored packages for project '{project_type}' are incorrect. Ignoring..."
+                f"Ignored packages for project '{project_type}' are incorrect. Ignoring...",
+                file=sys.stderr,
             )
             ignored_packages_map[project_type] = []
             continue
@@ -34,7 +37,8 @@ def get_ignored_packages(ignored_packages: dict):
         for ignored_package in ignored_packages:
             if not isinstance(ignored_package, str):
                 print(
-                    f"Ignored package for project {project_type}, '{ignored_package}' is incorrect. Ignoring..."
+                    f"Ignored package for project {project_type}, '{ignored_package}' is incorrect. Ignoring...",
+                    file=sys.stderr,
                 )
             else:
                 ignored_packages_map[project_type].append(ignored_package)
@@ -48,7 +52,8 @@ def get_licenses_whitelist(whitelist: list):
     for whitelisted_license in whitelist:
         if not isinstance(whitelisted_license, str):
             print(
-                f"Whitelisted license '{whitelisted_license}' is incorrect. Ignoring..."
+                f"Whitelisted license '{whitelisted_license}' is incorrect. Ignoring...",
+                file=sys.stderr,
             )
         else:
             licenses_whitelist.append(whitelisted_license)
@@ -69,7 +74,7 @@ def get_raw_config(path_to_config: str):
         with open(config_path) as file:
             return json.load(file)
     except FileNotFoundError:
-        print(f"Config file '{config_path}' not found...")
+        print(f"Config file '{config_path}' not found...", file=sys.stderr)
         return dict()
 
 
