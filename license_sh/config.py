da762868f777cd89ace42722443ebdcbbdcc559d
Bogdan Sikora <sikora.bogdan@gmail.com>
2020-02-05 11:50:56 +0100

    feat: get_config updated

--- license_sh/config.py
+++ license_sh/config.py
@@ -7,39 +7,53 @@ DEFAULT_CONFIG_NAME = ".license-sh.json"
 IGNORED_PACKAGES = "ignored_packages"
 WHITELIST = "whitelist"
 
+try:
+    from license_sh_private.licenses import COMMERCIAL_LICENSES as WHITELIST
+except ImportError:
+    PRIVATE_LICENCES_WHITELIST = []
+
+
 def get_ignored_packages(ignored_packages: dict):
-    ignored_packages_map = {
-            e.value: [] for e in ProjectType
-        }
+    ignored_packages_map = {e.value: [] for e in ProjectType}
     if not ignored_packages:
         return ignored_packages_map
     project_list = [e.value for e in ProjectType]
     for project_type, ignored_packages in ignored_packages.items():
         if not project_type in project_list:
-            print(f"Ignored packages for project '{project_type}' is not supported. Ignoring...")
+            print(
+                f"Ignored packages for project '{project_type}' is not supported. Ignoring..."
+            )
             continue
         if not isinstance(ignored_packages, list):
-            print(f"Ignored packages for project '{project_type}' are incorect. Ignoring...")
+            print(
+                f"Ignored packages for project '{project_type}' are incorrect. Ignoring..."
+            )
             ignored_packages_map[project_type] = []
             continue
-        
+
         for ignored_package in ignored_packages:
             if not isinstance(ignored_package, str):
-                print(f"Ignored package for project {project_type}, '{ignored_package}' is incorect. Ignoring...")
+                print(
+                    f"Ignored package for project {project_type}, '{ignored_package}' is incorrect. Ignoring..."
+                )
             else:
                 ignored_packages_map[project_type].append(ignored_package)
     return ignored_packages_map
 
+
 def get_licenses_whitelist(whitelist: list):
+    licenses_whitelist = PRIVATE_LICENCES_WHITELIST
     if not whitelist:
-        return []
-    licenses_whitelist = []
+        return licenses_whitelist
     for whitelisted_license in whitelist:
         if not isinstance(whitelisted_license, str):
-            print(f"Whitelisted license '{whitelisted_license}' is incorect. Ignoring...")
+            print(
+                f"Whitelisted license '{whitelisted_license}' is incorrect. Ignoring..."
+            )
         else:
             licenses_whitelist.append(whitelisted_license)
     return licenses_whitelist
+
 
 def get_config_path(path_to_config: str):
     return (
@@ -49,13 +63,23 @@ def get_config_path(path_to_config: str):
     )
 
 
-def get_config(path_to_config: str):
+def get_raw_config(path_to_config: str):
     config_path = get_config_path(path_to_config)
     try:
         with open(config_path) as file:
             return json.load(file)
     except FileNotFoundError:
-        return {}
+        print(f"Config file '{config_path}' not found...")
+        return dict()
+
+
+def get_config(path_to_config: str):
+    raw_config = get_raw_config(path_to_config)
+
+    return (
+        get_licenses_whitelist(raw_config.get(WHITELIST, [])),
+        get_ignored_packages(raw_config.get(IGNORED_PACKAGES, {})),
+    )
 
 
 def write_config(path_to_config: str, config):
@@ -69,6 +93,7 @@ def write_config(path_to_config: str, config):
 
 
 def whitelist_licenses(path_to_config: str, licenses: List[str]):
-    config = get_config(path_to_config)
-    config["whitelist"] = list(set(config.get("whitelist", []) + licenses))
+    config = get_raw_config(path_to_config)
+    current_whitelist = get_licenses_whitelist(config.get(WHITELIST, []))
+    config[WHITELIST] = list(set(current_whitelist + licenses))
     write_config(path_to_config, config)
